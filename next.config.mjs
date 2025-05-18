352f3930aaa1a76a1e173c17d327177b78bf6852
Vinayak Gaikwad <gaikwadvinayak291@gmail.com>
2025-05-18 22:01:19 +0530

    Update next config and gh pages deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/lstm-visualizer/' : '',
+  basePath: isProd ? '/lstm-visualizer' : '',
+  output: 'export'
+};
 
 export default nextConfig;
